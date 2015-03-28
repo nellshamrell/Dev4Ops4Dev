e4ef48f220edb16446a5efb4acb1cc13a7d99250
Clinton Wolfe <clinton@omniti.com>
2015-03-28 12:47:25 -0400

    Complete implementation of chaos.rb

--- tools/chaos.rb
+++ tools/chaos.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 
 require 'droplet_kit'
+require 'net/ssh'
 require 'optparse'
-require 'byebug'
 
 def main()
   opts = parse_command_line_opts()
@@ -16,8 +16,9 @@ def parse_command_line_opts()
   parsed_options[:environment] = 'testing'
   parsed_options[:mercy] = []
   parsed_options[:smite] = []
-  parsed_options[:wait] = 1000
+  parsed_options[:wait] = 1
   parsed_options[:chance] = 10
+  parsed_options[:service] = 'httpd'
 
   OptionParser.new do |cfg|
     cfg.banner = "Usage: chaos.rb [options]"
@@ -41,13 +42,18 @@ def parse_command_line_opts()
     end
 
     #   sleep time
-    cfg.on("-wMSEC", "--wait MSEC", "Wait MSEC milliseconds between droplets", OptionParser::DecimalInteger) do |w|
+    cfg.on("-wSEC", "--wait SEC", "Wait SEC seconds between droplets", OptionParser::DecimalInteger) do |w|
       parsed_options[:wait] = w
     end
 
     #   chance of chaos
     cfg.on("-cPCT", "--chance PCT", "Percent (1-100) chance of chaos.", OptionParser::DecimalInteger) do |c|
       parsed_options[:chance] = c
+    end
+
+    #   which service to hit
+    cfg.on("-SSVC", "--service SVC", "Name of service to kill") do |s|
+      parsed_options[:service] = s
     end
     
   end.parse!
@@ -66,6 +72,7 @@ def dropname2envname(dname)
 end
 
 def log (level, msg)
+  return if level == :debug
   time = DateTime.now().strftime('%H:%M:%S')
   puts sprintf('%5s - %s - %s',level.to_s.upcase(), time, msg) 
 end
@@ -98,15 +105,39 @@ def the_center_does_not_hold(opts)
 
     #   Loop over droplets
     drops.each do |drop|
-      log(:debug, "Examining droplet #{drop.name}")
-      # byebug
-#     Connect via SSH
-#        check for running service
-#          if running
-#            roll dice
-#            based on dice and smite
-#            halt service
-#     sleep
+      log(:info, "Examining droplet #{drop.name}")
+
+      #     Connect via SSH
+      ssh_opts = {
+        auth_methods: ['publickey'], # We are relying on the proper SSH key being in the agent
+        paranoid: false, # Disables strict hostkey checking
+      }
+      Net::SSH.start(drop.public_ip, 'root', ssh_opts) do |ssh|
+        #        check for running service
+        output = ssh.exec!("service #{opts[:service]} status")
+        log(:debug, "Service status output:\n\t#{output}")
+        #          if running
+        if output =~ /running, process/ then
+          #            roll dice
+          snake_eyes = rand(100) < opts[:chance]
+          doomed_anyway = opts[:smite].include?(dropname2groupname(drop.name))
+          #            based on dice and smite
+          if snake_eyes or doomed_anyway then            
+            #            halt service
+            log(:info, "           BURN ORDER!    ")
+            output = ssh.exec!("service #{opts[:service]} stop")
+            log(:info, " ~~~ sad trombone ~~~ (due to chaos)") if snake_eyes
+            log(:info, " ~~~ sad trombone ~~~ (due to smite)") if doomed_anyway
+            log(:debug, "Service halt output:\n\t#{output}")
+          end
+        else
+          log(:info, "Service #{opts[:service]} missing or not running")
+        end        
+      end
+
+      #     sleep
+      log(:debug, "Waiting a bit")
+      sleep(opts[:wait])
     end
   end
 end
