24e03f2ae44e95de49e9dc21bac2a7a2ba81211b
Clinton Wolfe <clinton@omniti.com>
2015-03-27 22:52:58 -0400

    Add code to convert course materials into HTML

--- usb-stick/download-usb-payload.sh
+++ usb-stick/download-usb-payload.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
+
+#===========================================================================================#
+#                                 "UI"
+#===========================================================================================#
 DOWNLOAD_THROTTLE=500k
 BOX_URL="https://www.dropbox.com/s/1o98s81qqz1mzkw/d4-workshop-0.4.0.box?dl=0"
+COURSE_REPO="git@github.com:nellshamrell/Dev4Ops4Dev.git"   # TODO - this is private
 
 # Debugging controls
 DO_DOWNLOADS=true
+DO_COURSE=true
+DO_REPORT=true
+
+#===========================================================================================#
+#                                  Preflight
+#===========================================================================================#
+
+if ! gem list -i github-markdown-preview >/dev/null; then
+    echo "Whoa, tiger.  I need you to run "
+    echo "    gem install github-markdown-preview"
+    echo " (may need sudo, rvm, rbenv, advil)"
+    exit 1
+fi
+
+#===========================================================================================#
+#                                  Downloads
+#===========================================================================================#
 
 if $DO_DOWNLOADS; then
     echo "=================================="
@@ -17,7 +39,7 @@ http://the.earth.li/~sgtatham/putty/latest/x86/putty-0.64-installer.exe
     for url in $URLS; do
         echo "--------------"
         echo -n '                      '
-        echo $url | cut -c 50-
+        echo $url | cut -c 48-
         CMD="wget"
         CMD="${CMD} --limit-rate $DOWNLOAD_THROTTLE" # Don't kill the hotel wifi
         CMD="${CMD} -c " # Continue interrupted downloads
@@ -28,7 +50,7 @@ http://the.earth.li/~sgtatham/putty/latest/x86/putty-0.64-installer.exe
     cd ..
 fi
 
-if true; then
+if $DO_DOWNLOADS; then
     echo "=================================="
     echo 'Fetching VirtualBox'
     mkdir -p VirtualBox
@@ -93,13 +115,55 @@ if $DO_DOWNLOADS; then
     $CMD $url
 fi
 
+#===========================================================================================#
+#                                    Materials Render to HTML
+#===========================================================================================#
 
-if which tree 2>/dev/null >/dev/null; then
-    tree -s
+if $DO_COURSE; then
+    echo "=================================="
+    echo 'Fetching the workshop Markdown files to convert to HTML'
+
+    mkdir -p course_html
+    cd course_html
+    if [[ ! -e repo/.git ]]; then
+        git clone $COURSE_REPO repo
+    else
+        cd repo && git pull > /dev/null && cd ..
+    fi
+
+    for f in $(find repo -name '*.md'); do
+        echo ".md => .html: $f"
+        # Great human UI! Terrible machine API!
+        github-markdown-preview $f >/dev/null 2>&1 &
+        STUPID=$!
+        sleep 1
+        kill -INT $STUPID
+    done
+
+    for f in $(find repo -name '*.html'); do
+        mv $f .
+    done
+    
+    rm -rf repo
+    cd ..
 fi
 
-du -h -s
 
+#===========================================================================================#
+#                                      Disk report
+#===========================================================================================#
+
+if $DO_REPORT; then
+    if which tree 2>/dev/null >/dev/null; then
+        tree -h
+    fi
+
+    du -h -s
+fi
+
+#===========================================================================================#
+#                                DO Creds reminder
+#===========================================================================================#
 
 # Use this to map the token to a list of SSH IDs
 # curl -X GET https://api.digitalocean.com/v2/account/keys -H "Authorization: Bearer $DIGITALOCEAN_ACCESS_TOKEN"
