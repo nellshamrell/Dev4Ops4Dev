750782f46806c8133dbf13770e371986c6cfe7e4
Clinton Wolfe <clinton@omniti.com>
2015-03-27 22:18:29 -0400

    Add notes on getting the DO SSH key ID into the stick

--- usb-stick/download-usb-payload.sh
+++ usb-stick/download-usb-payload.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
-# 0.4.0
+DOWNLOAD_THROTTLE=500k
 BOX_URL="https://www.dropbox.com/s/1o98s81qqz1mzkw/d4-workshop-0.4.0.box?dl=0"
 
-# TODO: windows aux tools
-# http://the.earth.li/~sgtatham/putty/latest/x86/putty-0.64-installer.exe
+# Debugging controls
+DO_DOWNLOADS=true
+
+if $DO_DOWNLOADS; then
+    echo "=================================="
+    echo 'Fetching Misc Tools'
+    mkdir -p misc
+    cd misc
+    URLS="
+http://the.earth.li/~sgtatham/putty/latest/x86/putty-0.64-installer.exe
+"
+    for url in $URLS; do
+        echo "--------------"
+        echo -n '                      '
+        echo $url | cut -c 50-
+        CMD="wget"
+        CMD="${CMD} --limit-rate $DOWNLOAD_THROTTLE" # Don't kill the hotel wifi
+        CMD="${CMD} -c " # Continue interrupted downloads
+        CMD="${CMD} -t 3" # Keep trying if interrupted
+        CMD="${CMD} -nv" # hush
+        $CMD $url
+    done
+    cd ..
+fi
 
 if true; then
     echo "=================================="
@@ -22,18 +44,17 @@ http://download.virtualbox.org/virtualbox/4.3.26/VirtualBox-4.3-4.3.26_98988_el7
         echo "--------------"
         echo -n '                      '
         echo $url | cut -c 50-
-        CMD="curl"
-        CMD="${CMD} -L" # Follow redirects
-        CMD="${CMD} -O" # Save response body in a file named like the URL file component
-        CMD="${CMD} -C -" # Continue interrupted downloads; errors on complete file, alas.
-        CMD="${CMD} --retry 3" # Keep trying if interrupted
-        CMD="${CMD} -Y 10240" # Abort and restart if rate falls below 10K for 30 sec
+        CMD="wget"
+        CMD="${CMD} --limit-rate $DOWNLOAD_THROTTLE" # Don't kill the hotel wifi
+        CMD="${CMD} -c " # Continue interrupted downloads
+        CMD="${CMD} -t 3" # Keep trying if interrupted
+        CMD="${CMD} -nv" # hush
         $CMD $url
     done
     cd ..
 fi
 
-if true; then
+if $DO_DOWNLOADS; then
     echo
     echo "=================================="
     echo 'Fetching Vagrant'
@@ -49,28 +70,27 @@ https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.2_x86_64.rpm
         echo "--------------"
         echo -n '                      '
         echo $url | cut -c 42-
-        CMD="curl"
-        CMD="${CMD} -L" # Follow redirects
-        CMD="${CMD} -O" # Save response body in a file named like the URL file component
-        CMD="${CMD} -C -" # Continue interrupted downloads; errors on complete file, alas.
-        CMD="${CMD} --retry 3" # Keep trying if interrupted
-        CMD="${CMD} -Y 10240" # Abort and restart if rate falls below 10K for 30 sec
+        CMD="wget"
+        CMD="${CMD} --limit-rate $DOWNLOAD_THROTTLE" # Don't kill the hotel wifi
+        CMD="${CMD} -c " # Continue interrupted downloads
+        CMD="${CMD} -t 3" # Keep trying if interrupted
+        CMD="${CMD} -nv" # hush
         $CMD $url
     done
     cd ..
 fi
 
-if true; then
+if $DO_DOWNLOADS; then
     echo
     echo "=================================="
     echo 'Fetching the Workshop Vagrant Box'
-    CMD="curl"
-    CMD="${CMD} -L" # Follow redirects
-    CMD="${CMD} -o workshop.box" # Save in filename of our choosing
-    CMD="${CMD} -C -" # Continue interrupted downloads; errors on complete file, alas.
-    CMD="${CMD} --retry 3" # Keep trying if interrupted
-    CMD="${CMD} -Y 10240" # Abort and restart if rate falls below 10K for 30 sec
-    $CMD $BOX_URL
+    CMD="wget"
+    CMD="${CMD} --limit-rate $DOWNLOAD_THROTTLE" # Don't kill the hotel wifi
+    CMD="${CMD} -O workshop.box " # specify filename
+    CMD="${CMD} -c " # Continue interrupted downloads
+    CMD="${CMD} -t 3" # Keep trying if interrupted
+    CMD="${CMD} -nv" # hush
+    $CMD $url
 fi
 
 
@@ -79,3 +99,12 @@ if which tree 2>/dev/null >/dev/null; then
 fi
 
 du -h -s
+
+
+# Use this to map the token to a list of SSH IDs
+# curl -X GET https://api.digitalocean.com/v2/account/keys -H "Authorization: Bearer $DIGITALOCEAN_ACCESS_TOKEN"
+
+echo "Be sure to also place the digitalocean-creds.txt file"
+echo "token:abababa..."
+echo "key_id:1234"
+
